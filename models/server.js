26fce5919b5b3eb0ea5382fe081f77fc28a76a11
David <davcastellanoslarios@gmail.com>
2021-11-10 09:23:49 -0600

    server ready at 09 23 am

--- models/server.js
+++ models/server.js
@@ -3,11 +3,20 @@ const express = require('express');
 class Server{
     constructor(){
         this.app = express();
+        this.port = process.env.PORT;
+
+        this.routes();
     }
 
     routes(){
         this.app.get('/', (req, res) => {
             res.send('hello world')
+        })
+    }
+
+    listen(){
+        this.app.listen(this.port, ()=>{
+            console.log('server runing in port', this.port);
         })
     }
 
