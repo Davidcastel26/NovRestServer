62a60f93c61c205c530870fa3d8059ffe7a34cba
David <davcastellanoslarios@gmail.com>
2021-11-10 09:21:30 -0600

    empty app js file since all the info is in server js file

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 require('dotenv').config()
 
-const express = require('express');
-const app = express();
 
-app.get('/', (req, res)=>{
-    res.send('hello world')
-})
 
-app.listen(process.env.PORT, ()=>{
-    console.log('server runing in port', process.env.PORT);
-})
+
+
