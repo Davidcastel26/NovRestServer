2110359ba5751d17d56e2e49e78750cdae7925db
David <davcastellanoslarios@gmail.com>
2021-11-10 09:36:27 -0600

    adding api rout

--- models/server.js
+++ models/server.js
@@ -18,7 +18,7 @@ class Server{
     }
 
     routes(){
-        this.app.get('/', (req, res) => {
+        this.app.get('/api', (req, res) => {
             res.send('hello world')
         })
     }
